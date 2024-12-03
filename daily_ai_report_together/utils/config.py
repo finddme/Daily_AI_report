f15e3e0b7ba3d7fc694cc723198c6b5ea23b5297
finddme <53667002+finddme@users.noreply.github.com>
2024-12-03 19:34:42 +0900

    Update config.py

--- daily_ai_report_together/utils/config.py
+++ daily_ai_report_together/utils/config.py
@@ -5,9 +5,9 @@ import openai
 
 # openai.api_key = os.getenv("OPENAI_API_KEY")
 claude_api_key = os.getenv("CLAUDE_API_KEY")
-openai.api_key = os.getenv("Together_API_KEY")
+together_api_key = os.getenv("Together_API_KEY")
 Claude_API_KEY=openai.api_key
-Together_API_KEY= openai.api_key
+Together_API_KEY= together_api_key
 
 openai_model_name="gpt-4o"
 claude_model_name="claude-3-5-sonnet-20240620"
